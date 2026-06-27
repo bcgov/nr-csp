8a83d686d86ccaec60fbd2509333ca2d9b369333
Kamal <kammoham@gov.bc.ca>
2026-06-27 01:03:33 -0600

    Update backend oracle connection

--- backend/src/main/java/ca/bc/gov/nrs/csp/backend/config/DataSourceConfig.java
+++ backend/src/main/java/ca/bc/gov/nrs/csp/backend/config/DataSourceConfig.java
@@ -12,26 +12,20 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-    @Bean
-    @Primary
-    public DataSource dataSource(
-            @Value("${spring.datasource.url}") String url,
-            @Value("${spring.datasource.username}") String username,
-            @Value("${spring.datasource.password}") String password,
-            @Value("${spring.datasource.driver-class-name:oracle.jdbc.OracleDriver}") String driverClass) {
-        HikariDataSource ds = DataSourceBuilder.create()
-                .type(HikariDataSource.class)
-                .url(url)
-                .username(username)
-                .password(password)
-                .driverClassName(driverClass)
-                .build();
-        // Oracle JDBC needs the wallet password to open ewallet.p12 (encrypted PKCS12 wallet).
-        // The wallet is created by the wallet-convert init container from the jssecacerts JKS.
-        String walletPassword = System.getenv("KEYSTORE_SECRET");
-        if (walletPassword != null && !walletPassword.isEmpty()) {
-            ds.addDataSourceProperty("oracle.net.wallet_password", walletPassword);
-        }
-        return new ValidatingDataSource(ds);
-    }
+  @Bean
+  @Primary
+  public DataSource dataSource(
+    @Value("${spring.datasource.url}") String url,
+    @Value("${spring.datasource.username}") String username,
+    @Value("${spring.datasource.password}") String password,
+    @Value("${spring.datasource.driver-class-name:oracle.jdbc.OracleDriver}") String driverClass) {
+    DataSource base = DataSourceBuilder.create()
+      .type(HikariDataSource.class)
+      .url(url)
+      .username(username)
+      .password(password)
+      .driverClassName(driverClass)
+      .build();
+    return new ValidatingDataSource(base);
+  }
 }
