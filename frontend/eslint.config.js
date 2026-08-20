e7df1015c1ff1ecc59df8ea106ebe02e3a3cc583
Kamal Mohammed <kamal.mohammed@outlook.com>
2026-08-19 21:37:01 -0600

    style: alphabetize eslint.config.js imports to satisfy import/order

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,11 @@
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import tanstackQuery from '@tanstack/eslint-plugin-query';
+import prettier from 'eslint-config-prettier';
 import importPlugin from 'eslint-plugin-import';
 import prettierPlugin from 'eslint-plugin-prettier';
-import prettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist/', 'build/', 'node_modules/'] },
